9d7d2d81575cba53934be303d36e6cab3ec16480
Rostyslav KrIt <rostyk1997@gmail.com>
2017-10-06 19:18:16 +0300

    dss

--- app/src/main/java/com/example/rkrit/test/activity/TextActivity.java
+++ app/src/main/java/com/example/rkrit/test/activity/TextActivity.java
@@ -1,13 +1,19 @@
 package com.example.rkrit.test.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.rkrit.test.R;
 
 public class TextActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,28 @@ public class TextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text);
         Intent intent = this.getIntent();
         ((TextView) findViewById(R.id.textView)).setText(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
+        imageView = (ImageView) findViewById(R.id.photo_smth);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        imageView.setOnClickListener((v)-> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 
 }
